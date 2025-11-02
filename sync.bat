3c114c740f3f9d370a9ce804ac3510680f1cf6c8
iamasink <iamasink@proton.me>
2025-11-02 16:49:25 +0000

    02/11/2025 16:49:25.62

--- sync.bat
+++ sync.bat
@@ -1,21 +1,38 @@
 @echo off
 
 rem --- Firefox ---
+echo.
+echo Updating user.js...
 set "FF_PROFILE=%APPDATA%\Mozilla\Firefox\Profiles\2kdzx0vu.default-release-1755453145240"
 
 copy /y "%FF_PROFILE%\user.js" "user.js.old"
 copy /y "user.js" "%FF_PROFILE%\user.js"
 
-rem --- Git ---
+echo.
+echo Running git add...
 git add .
-echo 
+
+echo.
+echo Committing...
 git commit -m "%date% %time%"
-echo 
+
+echo.
+echo Fetching upstream...
 git fetch upstream
-echo 
+
+echo.
+echo Switching to main...
 git checkout main
-echo 
+
+echo.
+echo Merging upstream/main...
 git merge upstream/main
-echo 
+
+echo.
+echo Pushing to origin...
 git push origin main
-echo 
+
+echo.
+echo All done :3
+echo.
+timeout /t 10
