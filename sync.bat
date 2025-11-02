cdd60579fc68e1cdf713b328605ffb56e0a1c24b
iamasink <iamasink@proton.me>
2025-11-02 16:48:15 +0000

    02/11/2025 16:48:15.03

--- sync.bat
+++ sync.bat
@@ -1,4 +1,4 @@
-@echo off
+rem @echo off
 
 rem --- Firefox ---
 set "FF_PROFILE=%APPDATA%\Mozilla\Firefox\Profiles\2kdzx0vu.default-release-1755453145240"
@@ -8,8 +8,14 @@ copy /y "user.js" "%FF_PROFILE%\user.js"
 
 rem --- Git ---
 git add .
+echo 
 git commit -m "%date% %time%"
+echo 
 git fetch upstream
+echo 
 git checkout main
+echo 
 git merge upstream/main
+echo 
 git push origin main
+echo 
